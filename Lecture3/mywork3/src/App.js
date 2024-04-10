e1e1741395aa160f1fee53d22dcd516ffb68c5df
rohichaudhari <137138335+rohichaudhari@users.noreply.github.com>
2024-04-10 19:32:04 +0530

    10/4/2024

---
+++ Lecture3/mywork3/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Styles from './Component/Styles';
+import './Exstyle.css'
+// import style1 from 'Para.module.css'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Styles/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Lecture3/mywork3/src/Component/Styles.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Styles() {
+  return (
+    <div>
+      <h1 style={{backgroundColor:"red",color:"white"}}>Reactjs is a javscript Library using a user Interface</h1>
+      <p className='text'>lorem ipsum decided free paragraph for small word type in lorem with you get word</p>
+      <h1>Hello I am rohini chaudhari, I belongs to vyara but i live in surat </h1>
+    </div>
+  )
+}
+
+export default Styles
