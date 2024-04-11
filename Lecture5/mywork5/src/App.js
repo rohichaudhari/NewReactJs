17fd7bf592fdeeb59f66d795aa63324c4ff53be1
rohichaudhari <137138335+rohichaudhari@users.noreply.github.com>
2024-04-11 22:29:11 +0530

    11/4/2024

---
+++ Lecture5/mywork5/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CompA from './Component/CompA';
+
+function App() {
+  return (
+    <div className="App">
+     <CompA/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Lecture5/mywork5/src/Component/CompC.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CompC() {
+  return (
+    <div>
+      <h1>Reactjs is javascript library</h1>
+    </div>
+  )
+}
+
+export default CompC
