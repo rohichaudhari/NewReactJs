4ad7a61689d9376f454c44b7eba1cf475a0ebdb4
rohichaudhari <137138335+rohichaudhari@users.noreply.github.com>
2024-04-14 15:30:18 +0530

    14/4/2024

---
+++ Lecture11/mywork11/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Props from './Component/Props';
+
+function App() {
+  return (
+    <div className="App">
+      <Props Name="Rohini" Lname="chaudhari"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Lecture11/mywork11/src/Component/Props.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Props(abc) {
+  return (
+    <div>
+      <h1>{abc.Name}</h1>
+      <p>{abc.Lname}</p>
+    </div>
+  )
+}
+
+export default Props
